87d18ec416a06c7f4ddbb2858abfca304a035897
JeffKGabriel <JeffKGabriel@gmail.com>
2017-09-06 20:07:18 -0400

    Update constants.js

--- app/config/constants.js
+++ app/config/constants.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase'
 
 var config = {
-  apiKey: "AIzaSyADZf64NHe0armH4qmJn4Oz6cpGJdpZNc0",
-  authDomain: "yokedin-9f501.firebaseapp.com",
-  databaseURL: "https://yokedin-9f501.firebaseio.com",
-  projectId: "yokedin-9f501",
-  storageBucket: "yokedin-9f501.appspot.com",
-  messagingSenderId: "932848231614"
+  apiKey: "0000000000000000000000000000000",
+  authDomain: "yokedin-00000.firebaseapp.com",
+  databaseURL: "https://yokedin-00000.firebaseio.com",
+  projectId: "yokedin-00000",
+  storageBucket: "yokedin-00000.appspot.com",
+  messagingSenderId: "000000000000"
 };
 firebase.initializeApp(config);
 
